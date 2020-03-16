4270a7a15ecbeafd1bd96a66192cf0f12dcab4b4
santistebanc <santistebanc94@gmail.com>
2020-03-16 01:19:31 +0100

    improve index logic

--- src/store2.js
+++ src/store2.js
@@ -1,21 +1,33 @@
 import {observable, transaction} from 'mobx';
-import {toObject, Index} from './utils';
+import {toObject, Index} from './utils2';
 import {computedFn} from 'mobx-utils';
+
+const Term = ({text, lang}) =>
+  observable({
+    text,
+    lang,
+    get sources() {
+      return Sources.getByTarget(this);
+    },
+  });
 
 export const Terms = observable({
   index: new Index(),
   add({text, lang, source}) {
-    const newItem = this.index.index([lang, text], {text, lang});
-    Sources.add({type: 'terms', source, target: newItem});
+    const newItem = this.index.set([lang, text], Term({text, lang}));
+    Sources.add({name: source, target: newItem});
     return newItem;
   },
 });
 
 export const Sources = observable({
   index: new Index(),
-  add({type, name, target}) {
-    return this.index.index([name, type, target]);
+  add({name, target}) {
+    return this.index.add([target], name);
   },
+  getByTarget: computedFn(function(target) {
+    return this.index.get([target]);
+  }),
 });
 
 export const Translations = observable({
@@ -26,34 +38,28 @@ export const Translations = observable({
         Object.entries(terms).forEach(([lang2, text2]) => {
           const term1 = Terms.add({text: text1, lang: lang1, source});
           const term2 = Terms.add({text: text2, lang: lang2, source});
-          const trans = this.index.index([term1, term2]);
-          Sources.add({type: 'translations', name: source, target: trans});
+          const trans = this.index.add([term1], term2);
+          Sources.add({name: source, target: trans});
         });
       });
     });
   },
   byText: computedFn(function(lang, text) {
     const term = Terms.index.get([lang, text]);
-    return term
-      ? [...this.index.get([term]).keys()].reduce(
-          (list, tr) =>
-            tr.lang !== lang
-              ? [
-                  ...list,
-                  {
-                    text: tr.text,
-                    lang: tr.lang,
-                  },
-                ]
-              : list,
-          [],
-        )
-      : [];
+    const list = [];
+    if (term) {
+      Array.from(this.index.get([term])).forEach(tr => {
+        if (tr.lang !== lang) {
+          list.push(tr);
+        }
+      });
+    }
+    return list;
   }),
   print() {
     return toObject(this.index.get(), ([term, trans]) => [
       term.text,
-      [...trans.keys()].map(tr => tr.text),
+      Array.from(trans).map(tr => tr.text),
     ]);
   },
 });
---
+++ src/utils2.js
@@ -0,0 +1,69 @@
+import {observable, action} from 'mobx';
+
+export const toObject = (map = new Map(), func) =>
+  Object.fromEntries(
+    Array.from(
+      map.entries(),
+      func ||
+        (([k, v]) => {
+          const vv = v.toJS ? v.toJS() : v;
+          const val = vv instanceof Map ? toObject(vv) : vv;
+          const kk = k.toJS ? k.toJS() : k;
+          const key =
+            kk instanceof Map
+              ? toObject(kk)
+              : typeof kk === 'object'
+              ? JSON.stringify(kk)
+              : kk;
+          return [key, val];
+        }),
+    ),
+  );
+
+const traverse = (map, path = []) => {
+  let current = map;
+  path.slice(0, -1).forEach(loc => {
+    if (!current.has(loc)) {
+      current.set(loc, new Map());
+    }
+    current = current.get(loc);
+  });
+  return current;
+};
+
+export class Index {
+  @observable store = new Map();
+  @action set(path = [], value) {
+    const last = path[path.length - 1];
+    const location = traverse(this.store, path);
+    if (!location.has(last)) {
+      location.set(last, value);
+    }
+    return location.get(last);
+  }
+  @action add(path = [], value) {
+    const last = path[path.length - 1];
+    const location = traverse(this.store, path);
+    if (!location.has(last)) {
+      location.set(last, new Set());
+    }
+    location.get(last).add(value);
+    return value;
+  }
+  get(path = []) {
+    let current = this.store;
+    for (const next of path) {
+      if (!current.has(next)) {
+        return;
+      }
+      current = current.get(next);
+    }
+    if (current.toJS && current.toJS() instanceof Set) {
+      return Array.from(current);
+    }
+    return current;
+  }
+  print(path = []) {
+    return toObject(this.get(path));
+  }
+}
